84fb8f20d53cbbc754030f4032739944cde91bcd
KarryCMT <1570606465@qq.com>
2020-03-13 15:18:13 +0800

    master

--- src/main.js
+++ src/main.js
@@ -1,8 +1,79 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
+} from 'element-ui'
 
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+axios.defaults.baseURL = 'http://39.97.124.157:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+Vue.prototype.$http = axios
 
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,56 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  }
-]
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/Login.vue') 
+    },
+    {
+      path: '/home',
+      component: () => import('../views/Home.vue'),
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: () => import('../views/Welcome.vue'),
+        },
+        {
+          path: '/users',
+          component: () => import('../views/user/Users.vue'),
+        },
+        {
+          path: '/rights',
+          component: () => import('../views/power/Rights.vue'),
+        },
+        {
+          path: '/roles',
+          component: () => import('../views/power/Roles.vue'),
+        }
+      ]
+    }
+  ]
+})
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
